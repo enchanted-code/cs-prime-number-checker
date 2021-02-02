8e18abd38cbf56d90afc2a200d6b21a966df7c73
Leo Spratt <enchant97@users.noreply.github.com>
2021-02-02 11:46:47 +0000

    add program code

--- Program.cs
+++ Program.cs
@@ -4,9 +4,53 @@ namespace prime_number_checker
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] _)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.Write("Enter Int To Check Or (q)uit: ");
+                string input = Console.ReadLine();
+                if (input.StartsWith("q"))
+                {
+                    break;
+                }
+                bool valid = int.TryParse(input, out int number);
+                if (!valid) {
+                    Console.WriteLine("Invalid Input");
+                    continue;
+                }
+                if (IsPrime(number))
+                {
+                    Console.WriteLine("Number Is Prime");
+                }
+                else
+                {
+                    Console.WriteLine("Number Is Not Prime");
+                }
+
+            }
+        }
+        /**
+         * <summary>check whether number is a prime</summary>
+         * <param name="number">int to check</param>
+         */
+        public static bool IsPrime(int number)
+        {
+            if (number > 1)
+            {
+                for (int i = 2; i < number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
